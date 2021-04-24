4ac2dab60999794ced284048126fe0a490aa4c82
Cameron Smith <theauspiciouscammy@gmail.com>
2021-04-23 21:26:12 -0700

    4/23 fundamentals commit

--- javaScript/01-Fundamentals/fundamentals.js
+++ javaScript/01-Fundamentals/fundamentals.js
@@ -217,7 +217,7 @@ var greaterThanArray = [1,3,5,7,9,13];
 //console.log("Total values greater than "+greaterThanArray[index]+" : "+greaterThan(greaterThanArray, index));
 
 //Write a function that accepts any array, and returns a new array with the array values that are greater than its 2nd value. Print how many values this is. What will you do if the array is only one element long?
-function greaterThan2(input, index){
+function greaterThanAny(input, index){
     if(input.length <=1){
         return "need more values than one.";
     }
@@ -230,7 +230,7 @@ function greaterThan2(input, index){
     }
     return sum;
 }
-//console.log("Total values greater than "+greaterThanArray[index]+" : "+greaterThan2(greaterThanArray, index));
+//console.log("Total values greater than "+greaterThanArray[index]+" : "+greaterThanAny(greaterThanArray, index));
 
 //Given two numbers, return array of length num1 with each value num2. Print "Jinx!" if they are same.
 function thisAndThat(num1, num2){
@@ -247,4 +247,170 @@ var that = 5;
 var that2 = 5;
 //console.log(thisAndThat(that, that2));
 
+//Your function should accept an array. If value at [0] is greater than array’s length, print "Too big!"; if value at [0] is less than array’s length, print "Too small!"; otherwise print "Just right!".
+function goldielocks(input){
+    if(input[0] > input.length){
+        return "Too big!";
+    }
+    else if(input[0] < input.length){
+        return "Too small!";
+    }
+    else{
+        return "Just right!";
+    }
+}
+var glArr = [4,2,3,4];
+//console.log(goldielocks(glArr));
 
+//Kelvin wants to convert between temperature scales. Create fahrenheitToCelsius(fDegrees) that accepts a number of degrees in Fahrenheit, and returns the equivalent temperature as expressed in Celsius degrees. For review, Fahrenheit = (9/5 * Celsius) + 32.
+function fahrenheitToCelsius(fDegrees){
+    var celsius = (fDegrees- 32) / (9/5);
+    return celsius;
+}
+var fDegrees = 14;
+//console.log(fahrenheitToCelsius(fDegrees));
+
+//Create celsiusToFahrenheit(cDegrees) that accepts number of degrees Celsius, and returns the equivalent temperature expressed in Fahrenheit degrees. (optional) Do Fahrenheit and Celsius values equate at a certain number? Scientific calculation can be complex, so for this challenge just try a series of Celsius integer values starting at 200, going downward (descending), checking whether it is equal to the corresponding Fahrenheit value.
+function celsiusToFahrenheit(cDegrees){
+    var fahrenheit = (9/5 * cDegrees) + 32;
+    return fahrenheit;
+}
+var cDegrees = -10;
+//console.log(celsiusToFahrenheit(cDegrees));
+
+//Given an array, write a function that changes all positive numbers in the array to “big”. Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1,"big","big",-5].
+function biggieSize(input){
+    for(var i = 0; i < input.length; i++){
+        if(input[i] > 0){
+            input[i] = "big";
+        }
+    }
+    return input;
+}
+var biggieArray = [-1,3,5,-5];
+//console.log(biggieSize(biggieArray));
+
+//You are passed an array containing strings. Working within that same array, replace each string with a number – the length of the string at previous array index – and return the array.
+function previeousLengths(input){
+    for(var i = input.length-1; i >= 0 ; i--){
+        if(i == 0){
+            input[i] = 0;
+            return input;
+        }
+        input[i] = input[i-1].length;
+    }
+    return input;
+}
+var plStrings = ["boo3", "haha", "gotchya", "kiddo"];
+//console.log(previeousLengths(plStrings));
+
+//Create a function that takes array of numbers. The function should print the lowest value in the array, and return the highest value in the array.
+function printLowReturnHigh(input){
+    var min = input[0];
+    var max = input[0];
+    for(var i = 0; i < input.length; i++){
+        if(input[i] < min)
+            min = input[i];
+        if(input[i] > max)
+            max = input[i];
+    }
+    console.log(min);
+    return max;
+}
+var pLrHArray = [-34,5,-5,24,14];
+//console.log(printLowReturnHigh(pLrHArray));
+
+//Build function that accepts array. Return a new array with all values except first, adding 7 to each. Do not alter the original array.
+function addSeven(input){
+    var newArray = [];
+    for(var i = 1; i < input.length; i++){
+        newArray.push(input[i]+7);
+    }
+    return newArray;
+}
+var add7Array = [2,1,3,4];
+//console.log(addSeven(add7Array));
+
+//Build a function that takes array of numbers. The function should print second-to-last value in the array, and return first odd value in the array.
+function printAndReturn(input){
+    console.log(input[input.length-2]);
+    for(var i = 0; i < input.length; i++){
+        if(input[i] % 2 != 0){
+            return input[i];
+        }
+    }
+}
+var pAndRArray = [1,14,3,4];
+//console.log(printAndReturn(pAndRArray));
+
+//Given array, write a function to reverse values, in-place. Example: reverse([3,1,6,4,2]) returns same array, containing [2,4,6,1,3].
+function reverseArray(input){
+    if(input.length < 2){
+        return input;
+    }
+    for(var i = 0; i < input.length/2; i++){
+        var temp = input[i];
+        input[i] = input[input.length-1-i];
+        input[input.length-1-i] = temp;
+    }
+    return input;
+}
+var reverseArayArray = [1,2,3,4,5,6];
+//console.log(reverseArray(reverseArayArray));
+
+//Given array, create a function to return a new array where each value in the original has been doubled. Calling double([1,2,3]) should return [2,4,6] without changing original.
+function doubleVision(input){
+    var newArray = [];
+    for(var i = 0; i < input.length; i++){
+        newArray.push(input[i]*2);
+    }
+    return newArray;
+}
+var dVisionArray = [2,3,4,5];
+//console.log(doubleVision(dVisionArray));
+
+//Given an array, create and return a new one containing all the values of the provided array, made negative (not simply multiplied by -1). Given [1,-3,5], return [-1,-3,-5].
+function negativeOutlook(input){
+    var newArray = [];
+    for(var i = 0; i < input.length; i++){
+        if(input[i] > 0){
+            newArray.push(input[i]*(-1));
+        }
+        else if(input[i] <= 0){
+            newArray.push(input[i]);
+        }
+    }
+    return newArray;
+}
+var negOutArray = [12,3,-5];
+//console.log(negativeOutlook(negOutArray));
+
+//Given array of numbers, create function to replace last value with number of positive values. Example, countPositives([-1,1,1,1]) changes array to [-1,1,1,3] and returns it.
+function endPositive(arr){
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] > 0){
+            sum += 1;
+        }
+    }
+    arr[arr.length-1] = sum;
+    return arr;
+}
+var endPosArray = [-1,1,1,1];
+//console.log(endPositive(endPosArray));
+
+//Create a function that accepts an array, and prints "yummy" each time one of the values is equal to "food". If no array elements are "food", then print "I'm hungry" once.
+function alwaysHungry(arr){
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] == "food"){
+            console.log("yummy");
+            sum += 1;
+        }
+    }
+    if(sum == 0){
+        console.log("I'm hungry");
+    }
+}
+var hungryArray = ["word", false, "food", 34, "no", "blah"];
+//alwaysHungry(hungryArray);
