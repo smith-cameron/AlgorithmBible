51e2ba3fe4c5a19ad3748bfc2a570771e3a69b13
Cameron Smith <theauspiciouscammy@gmail.com>
2021-05-30 13:55:27 -0700

    cleanup commit

--- python/03-Arrays/02-arrays.py
+++ python/03-Arrays/02-arrays.py
@@ -1,3 +1,4 @@
+#1 - Array: Min to Front
 def minFront(list):
     min = list[0]
     for i in range(len(list)):
@@ -9,5 +10,16 @@ def minFront(list):
             list[i] = list[i-1]
             list[i-1] = temp
     return list
-myList = [4,2,1,3,5]
-print(minFront(myList))
+myList = [4,2,1,3,5,6]
+#print(minFront(myList))
+
+#2 - Array: Reverse
+def reverse(list):
+    for i in range(len(list)/2):
+        temp = list[i]
+        list[i] = list[len(list)-1-i]
+        list[len(list)-1-i] = temp
+    return list
+print(reverse(myList))
+
+#3 - Array: Rotate
