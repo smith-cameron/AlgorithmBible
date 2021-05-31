2f0e658c0434371930a212b0046168a82df62663
Cameron Smith <theauspiciouscammy@gmail.com>
2021-05-31 08:12:03 -0700

    5/31

--- python/03-Arrays/02-arrays.py
+++ python/03-Arrays/02-arrays.py
@@ -4,13 +4,13 @@ def minFront(list):
     for i in range(len(list)):
         if list[i] < min:
             min = list[i]
-    for i in range(len(list)-1, 0, -1) :
-        if list[i] == min:
-            temp = list[i]
-            list[i] = list[i-1]
-            list[i-1] = temp
+            idx = i
+    for i in range(idx, 0, -1) :
+        temp = list[i]
+        list[i] = list[i-1]
+        list[i-1] = temp
     return list
-myList = [4,2,1,3,5,6]
+#myList = [4,2,1,3,5,6]
 #print(minFront(myList))
 
 #2 - Array: Reverse
@@ -20,6 +20,16 @@ def reverse(list):
         list[i] = list[len(list)-1-i]
         list[len(list)-1-i] = temp
     return list
-print(reverse(myList))
+#print(reverse(myList))
 
 #3 - Array: Rotate
+def rotate(list, shift):
+    for i in range(shift):
+        temp = list[len(list)-1]
+        for i in range(len(list)-1,0,-1):
+            list[i] = list[i-1]
+        list[0] = temp
+    return list
+aList = [1,2,3,4,5,6,7]
+offset = 3
+print(rotate(aList,offset))
