8e09023e5d6c7e69a1fcc20578abca61f643da19
Cameron Smith <theauspiciouscammy@gmail.com>
2021-06-26 13:39:24 -0700

    6/26

--- python/03-Arrays/04-arrays.py
+++ python/03-Arrays/04-arrays.py
@@ -1,1 +1,48 @@
+#1 - Array: Shuffle
 
+#2 - Array: Remove Range******
+def remove(input, idx):
+    output = []
+    if idx < 0:
+        for i in range(1,len(input)):
+            output.append(input[i])
+        return output
+    if idx > len(input):
+        for i in range(len(input)-1):
+            output.append(input[i])
+        return output
+    for i in range(len(input)):
+        if i != idx:
+            output.append(input[i])
+    return output
+def removeRange(input, idx1, idx2):
+    # for i in range(idx1, idx2):
+    #     input = remove(input,i)
+    #     print(input)
+    # return input
+    for i in range(idx1, len(input)):
+        if i <= idx2:
+            input = remove(input,i)
+        continue
+    return input
+#print(removeRange([20,30,40,50,60,70],2,4))
+#****** shift/skip problem.
+
+#3 - Intermediate Sums
+def intSums(input):
+    sum = 0
+    for i in range(len(input)):
+        sum += input[i]
+        print(sum)
+        if i % 10 == 0:
+            input.append(sum)
+            for i in range(len(input)-1, i,  -1):
+                temp = input[i]
+                input[i] = input[i-1]
+                input[i-1] = temp
+            sum = 0
+        elif i == len(input)-1:
+            input.append(sum)
+
+    return input
+print(intSums([1,2,1,2,1,2,1,2,1,2,1,2,1]))
