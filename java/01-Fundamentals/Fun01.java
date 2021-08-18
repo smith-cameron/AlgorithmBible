d86bc95f57475dae5c279ae3f51cc6ed184791d6
Cameron Smith <theauspiciouscammy@gmail.com>
2021-08-17 22:16:21 -0700

    01-Fun complete

---
+++ java/01-Fundamentals/Fun01.java
@@ -0,0 +1,143 @@
+class Fun01{
+    // 3
+    public void printNums() {
+		for(int i = -52; i <= 1066; i++) {
+			System.out.println(i);
+        }
+    }
+    // 4
+    public void printMult() {
+        int count = 0;
+		for(int i = 512; i <= 4096; i++) {
+            if(i % 5 == 0){
+                System.out.println(i);
+                count ++;
+            }
+        }
+        System.out.println(count);
+    }
+    // 5
+    public void printWhile(){
+        int i = 0;
+        while (i <= 60000) {
+            if (i % 6 == 0) {
+                System.out.println(i);
+            }
+            i ++;
+        }
+    }
+    // 6
+    public void beCheerful(int input){
+        for(int i = 0; i <= input; i++){
+            System.out.println("good morning!");
+        }
+    }
+    // 7
+    public void printEitherOr(){
+        for(int i = 0; i <= 100 ; i++){
+            if(i % 5 == 0 && i % 10 == 0){
+                System.out.println("Coding Dojo");
+            }
+            else if(i % 5 == 0){
+                System.out.println("Coding");
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+    // 8
+    public void printExcept(){
+        for(int i = -300; i <= 0; i+=3){
+            if(i == -6 || i == -3){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+    // 9
+    public void printParam(String input){
+        System.out.println(input);
+    }
+    // 10
+    public void printIntegers(){
+        int i = 2000;
+        while(i <= 5280){
+            System.out.println(i);
+            i++;
+        }
+    }
+    // 11
+    public int addOdds(){
+        int sum = 0;
+		for(int i = -300000; i <= 300000; i++) {
+            if( i % 2 != 0){
+                sum += i;
+                System.out.println(sum);
+            }
+		}
+		return sum;
+    }
+    // 12
+    public void cakeDay(int num1, int num2){
+        if (num1 == 12 || num1 == 21){
+            if(num2 == 12 || num2 == 21){
+                System.out.println("How did you know!?");
+            }
+            else {
+                System.out.println("Just another day....");
+            }
+        }
+        else {
+            System.out.println("Just another day....");
+        }
+    }
+    // 13
+    public void printDown(){
+        int i = 2016;
+        while(i >= 1){
+            System.out.println(i);
+            i -= 4;
+        }
+    }
+    // 14
+    public void flexibleCountdown(int min, int max, int mult){
+        for(int i = max; i >= min; i -= mult) {
+            System.out.println(i);
+		}
+    }
+    // 15
+    public void leapYear(int input){
+        if (input % 4 == 0){
+            if (input % 100 == 0){
+                if (input % 400 == 0){
+                    System.out.println("A Special Kind of Leap Year!");
+                }
+                else{
+                    System.out.println("Not a Leap Year.");
+                }
+            }
+            else{
+                System.out.println("Leap Year!");
+            }
+        }
+        else{
+            System.out.println("Not a Leap Year.");
+        }
+    }
+    // 16
+    public void finalCountdown(int mult, int min, int max, int exclude){
+        while(min <= max){
+            if(min % mult ==0){
+                if(min == exclude){
+                    min += 1;
+                    continue;
+                }
+                else{
+                    System.out.println(min);
+                }
+            }
+            min +=1;
+        }
+    }
+}
---
+++ java/01-Fundamentals/FunCon01.java
@@ -0,0 +1,20 @@
+class FunCon01 {
+    public static void main(String[] args) {
+        Fun01 algos = new Fun01();
+        // algos.printNums();
+        // algos.printMult();
+        // algos.printWhile();
+        // algos.beCheerful(98);
+        // algos.printEitherOr();
+        // algos.printExcept();
+        // algos.printParam("HIIIIII");
+        // algos.printIntegers();
+        // System.out.println(algos.addOdds());
+        // algos.cakeDay(21, 24);
+        // algos.printDown();
+        // algos.flexibleCountdown(2,9,3);
+        // algos.leapYear(2000);
+        algos.finalCountdown(3,5,17,9);
+
+    }
+}
