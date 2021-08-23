7eb341a2af9afa38921ac48496f87c10ab19e95e
Cameron Smith <theauspiciouscammy@gmail.com>
2021-08-23 10:43:30 -0700

    update

--- java/01-Fundamentals/Fun03.java
+++ java/01-Fundamentals/Fun03.java
@@ -1,12 +1,8 @@
+import java.util.ArrayList;
 public class Fun03 {
-    public static void main(String[] args) {
-        int[] inputRH = {2,4,3,8,5,7};
-        System.out.println(returnHigh(inputRH));
-
-    }
     // 1 & 2 Not able to mix array data types in java
     // 3
-    public static int returnHigh(int[] input){
+    public int returnHigh(int[] input){
         int max = input[0];
         int min = input[0];
         for(int i = 0; i < input.length; i++){
@@ -19,5 +15,19 @@ public class Fun03 {
         }
         System.out.println(min);
         return max;
+    }
+    // 4
+    public ArrayList<Integer> addSeven(int[] input){
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        for(int i = 1; i < input.length; i++){
+            output.add(input[i] + 7);
+        }
+        return output;
+    }
+    // 5
+    public int printAreturnB(int[] input){
+        int firstOdd = 0;
+        
+        return firstOdd;
     }
 }
---
+++ java/01-Fundamentals/FunCon03.java
@@ -0,0 +1,9 @@
+public class FunCon03 {
+    public static void main(String[] args) {
+        Fun03 algo = new Fun03();
+        int[] inputRH = {2,4,3,8,5,7};
+        System.out.println(algo.returnHigh(inputRH));
+        System.out.println(algo.addSeven(inputRH));
+
+    }
+}
