6c52a9d52a33b6f1ee967665602238f1679c5a7c
theauspiciouscammy <theauspiciouscammy@gmail.com>
2021-10-19 15:47:32 -0700

    update

--- Algorithms/00-DebuggingChallenges/01-Buggy13.js
+++ Algorithms/00-DebuggingChallenges/01-Buggy13.js
@@ -5,4 +5,46 @@ console.log(num);
 num = num + 1;
 }
 }
-print1to255()
+// print1to255()
+
+function printMaxOfArray(arr) {
+    if (arr.length == 0) {
+        console.log("[], no max val.");     
+        return;
+    }
+    var max = 0;
+    for (var idx = 0; idx < arr.length; idx++) {
+        if (arr[idx] > max){ 
+            max = arr[idx]; 
+        }
+    }
+    console.log("Max val is:", max);
+}
+
+function printMaxOfArray(arr) {
+    if (arr.length == 0) {
+    console.log("[], no max val.");
+    return;
+    }
+    var max = 0;
+    for (var idx = 0;
+    idx < arr.length; idx++) {
+    if (arr[idx] > max)
+    { max = arr[idx]; }
+    }
+    console.log("Max val is:", max);
+    }
+    
+printMaxOfArray([])
+
+
+function printOdds1to255()
+{
+var num = 1;
+while (num <= 255)
+{
+console.log(num + 2);
+}
+num ++
+}
+printOdds1to255()
